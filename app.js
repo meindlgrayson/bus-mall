0efa56046fb2f8c485e1285b54c915242321dbf8
F Grayson Meindl <meindlgrayson@gmail.com>
2017-02-20 14:26:11 -0800

    added image files to js

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var images = [
+  'img/bag.jpg',
+  'img/banana.jpg',
+  'img/bathroom.jpg',
+  'img/boots.jpg',
+  'img/breakfast.jpg',
+  'img/bubblegum.jpg',
+  'img/chair.jpg',
+  'img/cthulhu.jpg',
+  'img/dog-duck.jpg',
+  'img/dragon.jpg',
+  'img/pen.jpg',
+  'img/pet-sweep.jpg',
+  'img/scissors.jpg',
+  'img/shark.jpg',
+  'img/sweep.png',
+  'img/tauntaun.jpg',
+  'img/unicorn.jpg',
+  'img/usb.gif',
+  'img/water-can.jpg',
+  'img/wine-glass.jpg'];
+
+var first = document.getElementById('image_one');
+var second = document.getElementById('image_two');
+var third = document.getElementById('image_three');
+
+first.src = images[0];
+second.src = images[1];
+third.src = images[2];
+
