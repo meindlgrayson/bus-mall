fbd0e5fa03d4acce497898f4e54cfcd57461c407
F Grayson Meindl <meindlgrayson@gmail.com>
2017-02-21 08:46:10 -0800

    photos randomly generate with no identical photos on same lineup. clicked photos stored in array

--- app.js
+++ app.js
@@ -22,11 +22,63 @@ var images = [
   'img/water-can.jpg',
   'img/wine-glass.jpg'];
 
+var itemClicked = [];
+
 var first = document.getElementById('image_one');
 var second = document.getElementById('image_two');
 var third = document.getElementById('image_three');
 
-first.src = images[0];
-second.src = images[1];
-third.src = images[2];
+function generateNum() {
+  var imageNumOne = Math.floor(Math.random() * images.length);
+  console.log(imageNumOne);
+  first.src = images[imageNumOne];
+  var imageNumTwo = Math.floor(Math.random() * images.length);
+  while(imageNumOne === imageNumTwo) {
+    imageNumTwo = Math.floor(Math.random() * images.length);
+  }
+  second.src = images[imageNumTwo];
+  var imageNumThree = Math.floor(Math.random() * images.length);
+  while (imageNumThree === imageNumOne || imageNumThree === imageNumTwo) {
+    imageNumThree = Math.floor(Math.random() * images.length);
+  }
+  third.src = images[imageNumThree];
+}
+generateNum();
 
+var clickArea = document.getElementById('photos');
+clickArea.addEventListener('click', clickHandler);
+
+var repeats = [];
+
+function clickHandler(event) {
+  console.log(event.target.getAttribute('src'));
+  itemClicked.push(event.target.getAttribute('src'));
+  if (itemClicked.length <= 25) {
+    generateNum();
+  } else {
+    alert('test complete'); /*WHAT HAPPENS AFTER TEST*/
+    for(var i = 0; i < itemClicked.length; i++) {
+      for(var j = i + 1; j < itemClicked.length; j++) {
+        if(itemClicked[i] === itemClicked[j]) {
+          console.log(itemClicked[i]);
+          repeats.push(itemClicked[i]);
+        }
+      }
+    }
+  }
+}
+// var count = 0;
+
+// for (var i = 0; i < repeats.length; i++) {
+//   for (var j = 0; j < repeats.length; j++) {
+//     if (repeats[i] === repeats[j]) {
+//       count += 1;
+//       var multiClick = new Results(String(repeats[j]), count);
+//     }
+//   }
+// }
+
+// function Results(name, times) {
+//   this.imageFile = name;
+//   this.timesClicked = times; 
+// }
