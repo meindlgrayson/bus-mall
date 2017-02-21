dfa293a1c5ab6e0ad77a5295d4e385d7d2622a62
F Grayson Meindl <meindlgrayson@gmail.com>
2017-02-21 15:34:18 -0800

    created a constructor and made objects for all images. clicks and shows tracked within objects. no repeated images on next click.

--- app.js
+++ app.js
@@ -1,84 +1,119 @@
 'use strict';
 
+function Content(source) {
+  this.path = 'img/' + source;
+  this.name = source.substr(0, source.length - 4);
+  this.shown = 0;
+  this.clicked = 0;
+}
+var objectImages = [];
 var images = [
-  'img/bag.jpg',
-  'img/banana.jpg',
-  'img/bathroom.jpg',
-  'img/boots.jpg',
-  'img/breakfast.jpg',
-  'img/bubblegum.jpg',
-  'img/chair.jpg',
-  'img/cthulhu.jpg',
-  'img/dog-duck.jpg',
-  'img/dragon.jpg',
-  'img/pen.jpg',
-  'img/pet-sweep.jpg',
-  'img/scissors.jpg',
-  'img/shark.jpg',
-  'img/sweep.png',
-  'img/tauntaun.jpg',
-  'img/unicorn.jpg',
-  'img/usb.gif',
-  'img/water-can.jpg',
-  'img/wine-glass.jpg'];
+  'bag.jpg',
+  'banana.jpg',
+  'bathroom.jpg',
+  'boots.jpg',
+  'breakfast.jpg',
+  'bubblegum.jpg',
+  'chair.jpg',
+  'cthulhu.jpg',
+  'dog-duck.jpg',
+  'dragon.jpg',
+  'pen.jpg',
+  'pet-sweep.jpg',
+  'scissors.jpg',
+  'shark.jpg',
+  'sweep.png',
+  'tauntaun.jpg',
+  'unicorn.jpg',
+  'usb.gif',
+  'water-can.jpg',
+  'wine-glass.jpg'];
+
+for (var i = 0; i < images.length; i++) {
+  var newObject = new Content(images[i]);
+  objectImages.push(newObject);
+}
+console.log(objectImages);
 
 var itemClicked = [];
+var itemShown = [];
 
 var first = document.getElementById('image_one');
 var second = document.getElementById('image_two');
 var third = document.getElementById('image_three');
 
 function generateNum() {
-  var imageNumOne = Math.floor(Math.random() * images.length);
-  console.log(imageNumOne);
-  first.src = images[imageNumOne];
-  var imageNumTwo = Math.floor(Math.random() * images.length);
-  while(imageNumOne === imageNumTwo) {
-    imageNumTwo = Math.floor(Math.random() * images.length);
-  }
-  second.src = images[imageNumTwo];
-  var imageNumThree = Math.floor(Math.random() * images.length);
-  while (imageNumThree === imageNumOne || imageNumThree === imageNumTwo) {
-    imageNumThree = Math.floor(Math.random() * images.length);
-  }
-  third.src = images[imageNumThree];
+  return Math.floor(Math.random() * objectImages.length);
 }
-generateNum();
+
+function generateFirstNum() {
+  var imageNumOne = generateNum();
+  console.log(imageNumOne);
+  first.src = objectImages[imageNumOne].path;
+  itemShown.unshift(objectImages[imageNumOne].path);
+  var imageNumTwo = generateNum();
+  while(imageNumOne === imageNumTwo) {
+    imageNumTwo = generateNum();
+  }
+  second.src = objectImages[imageNumTwo].path;
+  itemShown.unshift(objectImages[imageNumTwo].path);
+  var imageNumThree = generateNum();
+  while (imageNumThree === imageNumOne || imageNumThree === imageNumTwo) {
+    imageNumThree = generateNum();
+  }
+  third.src = objectImages[imageNumThree].path;
+  itemShown.unshift(objectImages[imageNumThree].path);
+}
+generateFirstNum();
+
+function generateFollowing() {
+  var imageNumOne = generateNum();
+  console.log(imageNumOne);
+  while(objectImages[imageNumOne].path === itemShown[0] || objectImages[imageNumOne].path === itemShown[1] || objectImages[imageNumOne].path === itemShown[2]){
+    imageNumOne = generateNum();
+  }
+  first.src = objectImages[imageNumOne].path;
+  itemShown.unshift(objectImages[imageNumOne].path);
+  var imageNumTwo = generateNum();
+  while(imageNumOne === imageNumTwo || objectImages[imageNumTwo].path === itemShown[0] || objectImages[imageNumTwo].path === itemShown[1] || objectImages[imageNumTwo].path === itemShown[2]) {
+    imageNumTwo = generateNum();
+  }
+  second.src = objectImages[imageNumTwo].path;
+  itemShown.unshift(objectImages[imageNumTwo].path);
+  var imageNumThree = generateNum();
+  while (imageNumThree === imageNumOne || imageNumThree === imageNumTwo || objectImages[imageNumThree].path === itemShown[0] || objectImages[imageNumThree].path === itemShown[1] || objectImages[imageNumThree].path === itemShown[2]) {
+    imageNumThree = generateNum();
+  }
+  third.src = objectImages[imageNumThree].path;
+  itemShown.unshift(objectImages[imageNumThree].path);
+}
+
 
 var clickArea = document.getElementById('photos');
 clickArea.addEventListener('click', clickHandler);
 
-var repeats = [];
 
 function clickHandler(event) {
   console.log(event.target.getAttribute('src'));
   itemClicked.push(event.target.getAttribute('src'));
   if (itemClicked.length <= 25) {
-    generateNum();
+    generateFollowing();
   } else {
     alert('test complete'); /*WHAT HAPPENS AFTER TEST*/
-    for(var i = 0; i < itemClicked.length; i++) {
-      for(var j = i + 1; j < itemClicked.length; j++) {
-        if(itemClicked[i] === itemClicked[j]) {
-          console.log(itemClicked[i]);
-          repeats.push(itemClicked[i]);
+    for (var i = 0; i < itemShown.length; i++) {
+      for (var j = 0; j < objectImages.length; j++) {
+        if (itemShown[i] === objectImages[j].path) {
+          objectImages[j].shown += 1;
+        }
+      }
+    }
+    for (var k = 0; k < itemClicked.length; k++) {
+      for (var l = 0; l < objectImages.length; l++) {
+        if (itemClicked[k] === objectImages[l].path) {
+          objectImages[l].clicked += 1;
         }
       }
     }
   }
 }
-// var count = 0;
 
-// for (var i = 0; i < repeats.length; i++) {
-//   for (var j = 0; j < repeats.length; j++) {
-//     if (repeats[i] === repeats[j]) {
-//       count += 1;
-//       var multiClick = new Results(String(repeats[j]), count);
-//     }
-//   }
-// }
-
-// function Results(name, times) {
-//   this.imageFile = name;
-//   this.timesClicked = times; 
-// }
